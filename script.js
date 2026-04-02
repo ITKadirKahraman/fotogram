e7861761d42e4a0a1d411133bf90e43c0794e622
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-04-02 19:55:43 +0200

    Fix typos and update dialog functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let dialogREF = document.getElementById("fullPhoto");
-let dialogInitPhoto = ducument.getElementById('photoGallary');
+let dialogInitPhoto = document.getElementById('photoGallary');
+let columnLayout = document.getElementById('gridLayout');
 let closeDialog = document.getElementById('closeDialog');
 let dialogHeadline = document.getElementById('dialogHeadline');
 let dialogPhoto = document.getElementById('dialogPhoto');
@@ -16,7 +17,7 @@ let allImages =
         {id: 3, name: "Das was ich sehr gerne Essen.", link: "img/doener.svg", alt: "Dönertasche"},
         {id: 4, name: "Das ist meine Galaxie, mein Wohnort der Zukunft.", link: "img/galaxie.svg", alt: "Galaxie"},
         {id: 5, name: "Mein Traumkörper ist es ein Sixpack zu haben.", link: "img/gym.svg", alt: "Gym"},
-        {id: 6, name: "Das Land, was ich gerne bereisen möchte.", link: img/japan.svg, alt: "Japan"},
+        {id: 6, name: "Das Land, was ich gerne bereisen möchte.", link: "img/japan.svg", alt: "Japan"},
         {id: 7, name: "Ein MacBook für die Arbeit.", link: "img/macbook.svg", alt: "MacBook"},
         {id: 8, name: "Wenn es um Teigwaren geht, esse ich gerne Pizza.", link: "img/pizza.svg", alt: "Pizza"},
         {id: 9, name: "Italien? Spaghetti!", link: "img/spaghetti.svg", alt: "Spaghetti"},
@@ -30,62 +31,25 @@ function initGallary() {
     for(let index = 0; index < allImages.length; index++){
         dialogInitPhoto.innerHTML += photoTemplet(index);
     }
+    updateLayout();
+}
+
+function updateLayout() {
+    if(allImages.length > 7){
+        dialogInitPhoto.classList.add("gridLayout");
+    }
 }
 
 function photoTemplet(index){
     return `<section>
-            <div>
-            <button onclick="initDialog(${allImages[index].id})" aria-haspopup="dialog">
-            <h2 ${allImages[index].name} class="h2"></h2>
-            <img src="${allImages[index].link}" alt="${allImages[index].alt}">
-            </button>
-            </div>
+                <div>
+                    <button onclick="openDialog(${allImages[index].id})" aria-haspopup="dialog">
+                        <img src="${allImages[index].link}" alt="${allImages[index].alt}">
+                    </button>
+                </div>
             </section>`
-}
-
-function renderDialog(photoID) {
-    for(let index = 0; index < allImages.length; index++)
-    {
-        if(photoID == allImages[index].id)
-        {
-            // ?
-        }
-    }
-}
-
-function initDialog(photoId) {
-    renderDialog(photoId);
-    openDialog();
 }
 
 function openDialog() {
     dialogREF.showModal();
 }
-
-function fillDialog(i) {
-    dialogHeadline.innerHTML = allImages[i].name;
-    dialogPhoto.scr = allImages[i].link;
-    dialogPhoto.alt = allImages[i].alt;
-    dialogPageNumber.innerHTML = allImages[i].id + " / " + allImages.length;
-}
-
-dialogREF.addEventListener("click", (event) => {
-    if(event.target == dialogREF || event.target == closeDialog) {
-        dialogREF.close();
-        initGallary();
-    }
-
-    if(event.target == dialogLeft){
-        dialogID--;
-        if(dialogID < 1){
-            dialogID = allImages.length;
-        }
-        renderDialog(dialogID);
-    }else if(event.target == dialogRight){
-        dialogID++;
-        if(dialogID > allImages.length){
-            dialogID = 1;
-        }
-        renderDialog(dialogID);
-    }
-});
